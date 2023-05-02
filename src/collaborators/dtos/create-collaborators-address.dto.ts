cd8096959f2dee3ff7fd919af4b5435f6c5aacea
Jin B. Oliveira <jinoliveira74@gmail.com>
2023-05-02 18:19:00 -0300

    add: class validadtor criar colaborador

--- src/collaborators/dtos/create-collaborators-address.dto.ts
+++ src/collaborators/dtos/create-collaborators-address.dto.ts
@@ -1,11 +1,33 @@
+import { IsNumber, IsOptional, IsString, IsUUID } from 'class-validator';
+
 export class CreateCollaboratorsAddressDto {
+  @IsOptional()
+  @IsUUID()
   id?: string;
+
+  @IsString()
   zip_code: string;
+
+  @IsString()
   street: string;
+
+  @IsOptional()
+  @IsString()
   complement: string;
+
+  @IsString()
   neighborhood: string;
+
+  @IsString()
   fu: string;
+
+  @IsString()
   location: string;
+
+  @IsNumber()
   number: number;
+
+  @IsOptional()
+  @IsString()
   collaborator_id?: string;
 }
--- src/collaborators/dtos/create-collaborators-profile.dto.ts
+++ src/collaborators/dtos/create-collaborators-profile.dto.ts
@@ -1,3 +1,5 @@
+import { IsDate, IsEnum, IsOptional, IsString, IsUUID } from 'class-validator';
+
 enum Gender {
   MASCULINO = 'MASCULINO',
   FEMININO = 'FEMININO',
@@ -15,21 +17,57 @@ enum ContractRegime {
 }
 
 export class CreateCollaboratorsProfileDto {
+  @IsOptional()
   collaborator_id: string;
+
+  @IsString()
   rg: string;
+
+  @IsString()
   cpf: string;
+
+  @IsString()
   birth_date: Date;
+
+  @IsEnum(Gender)
   gender: Gender;
+
+  @IsEnum(FrontBackSales)
   f_b_s: FrontBackSales;
+
+  @IsUUID()
   branch_id: string;
+
+  @IsUUID()
   team_id: string;
+
+  @IsUUID()
   role_id: string;
+
+  @IsString()
   payment_bank: string;
+
+  @IsString()
   bank_agency: string;
+
+  @IsString()
   bank_account: string;
+
+  @IsEnum(ContractRegime)
   contract_regime: ContractRegime;
+
+  @IsString()
+  @IsOptional()
   previous_company: string;
+
+  @IsString()
   av_entry_date: Date;
+
+  @IsString()
+  @IsOptional()
   xp_entry_date: Date;
+
+  @IsString()
+  @IsOptional()
   av_departure_date: Date;
 }
