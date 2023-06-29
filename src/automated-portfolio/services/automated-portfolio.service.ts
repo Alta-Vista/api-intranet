8399cf5d1213980a3ffe31d7f4ff786953fa751b
Jin B. Oliveira <jinoliveira74@gmail.com>
2023-06-29 14:25:51 -0300

    remove: notificações

--- src/automated-portfolio/services/automated-portfolio.service.ts
+++ src/automated-portfolio/services/automated-portfolio.service.ts
@@ -6,13 +6,11 @@ import {
   ListRequestedAssetsDto,
 } from '../dto';
 import { ListRequestsDto } from '../dto/list-requests.dto';
-import { EventEmitter2 } from '@nestjs/event-emitter';
 
 @Injectable()
 export class AutomatedPortfolioService {
   constructor(
     private automatedPortfolioRepository: AutomatedPortfolioRepository,
-    private readonly eventEmitter: EventEmitter2,
   ) {}
 
   async create(data: CreateAutomatedPortfolioRequestDto, advisorCode?: string) {
@@ -23,16 +21,6 @@ export class AutomatedPortfolioService {
       client: data.client,
       message: data.message,
     });
-
-    const payload = {
-      name: '',
-      to: 'caue.glorgiano@altavistainvest.com.br',
-      message:
-        'Um assessor mandou um novo cliente para a carteira automatizada, para ver o cliente e os ativos, entre na Intranet!',
-      subject: '[MESA RV] - Carteira automatizada solicitações',
-    };
-
-    this.eventEmitter.emit('notification.send-notification', payload);
   }
 
   async listClientPortfolio(
